ebd7710493deba229f366d40d766a0c987731e6e
cme95 <auddms617@gmail.com>
2025-05-01 23:16:36 +0900

    memo: tailwindcss, shadcnui 설정

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,10 @@
+import { Button } from "~/components/ui/button";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome"; // Vite dev 서버의 alias 캐싱 문제가 존재하는 듯 // "../" 로 오류나면 "~/" 로 바꾸기
-
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
 
 export default function Home() {
-  return <Welcome />;
+  return (
+      <h1>
+        home <Button>Click me</Button>
+      </h1>
+  );
 }
--- react-router.config.ts
+++ react-router.config.ts
@@ -1,5 +1,7 @@
 import type { Config } from "@react-router/dev/config";
 
+/** 프레임워크 설정 */
+
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
