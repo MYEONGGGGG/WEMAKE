f852339e8afbec07aff89a7e798ec3337e9a15fc
cme95 <auddms617@gmail.com>
2025-05-01 22:19:44 +0900

    memo: routes.ts의 역할

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,10 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+/**
+ * routes.ts 파일은 React router가 웹사이트에 어떤 페이지가 있는지 확인하는 곳(사용자가 이동할 수 있는 URL)
+ * */
+
+export default [
+    index("routes/home.tsx"),
+    route("/about", "potato/tomato.tsx"),
+] satisfies RouteConfig;
