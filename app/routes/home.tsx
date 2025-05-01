15a1b7a6d8ce31a26eede6875182ce1a2125202e
cme95 <auddms617@gmail.com>
2025-05-01 22:11:15 +0900

    memo: Vite dev 실행 시, alias 캐싱 문제 발생하니 주의

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,5 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Welcome } from "../welcome/welcome"; // Vite dev 서버의 alias 캐싱 문제가 존재하는 듯 // "../" 로 오류나면 "~/" 로 바꾸기
 
 export function meta({}: Route.MetaArgs) {
   return [
