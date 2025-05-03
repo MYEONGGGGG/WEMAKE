70c68ee52b403fb6329b937a0f411ff3dafec730
cme95 <auddms617@gmail.com>
2025-05-03 23:49:22 +0900

    memo: navigation 컴포넌트 생성

---
+++ app/common/components/index.ts
@@ -0,0 +1,35 @@
+export { Avatar, AvatarImage, AvatarFallback } from "~/common/components/ui/avatar";
+
+export { Button, buttonVariants } from "~/common/components/ui/button";
+
+export {
+    DropdownMenu,
+    DropdownMenuPortal,
+    DropdownMenuTrigger,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuLabel,
+    DropdownMenuItem,
+    DropdownMenuCheckboxItem,
+    DropdownMenuRadioGroup,
+    DropdownMenuRadioItem,
+    DropdownMenuSeparator,
+    DropdownMenuShortcut,
+    DropdownMenuSub,
+    DropdownMenuSubTrigger,
+    DropdownMenuSubContent,
+} from "~/common/components/ui/dropdown-menu";
+
+export {
+    NavigationMenu,
+    NavigationMenuList,
+    NavigationMenuItem,
+    NavigationMenuContent,
+    NavigationMenuTrigger,
+    NavigationMenuLink,
+    NavigationMenuIndicator,
+    NavigationMenuViewport,
+    navigationMenuTriggerStyle,
+} from "~/common/components/ui/navigation-menu";
+
+export { Separator } from "~/common/components/ui/separator";
