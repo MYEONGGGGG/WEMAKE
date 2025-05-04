c90c025fa06a0cb2902f9c0cd3bd288c57cc609c
cme95 <auddms617@gmail.com>
2025-05-04 17:45:41 +0900

    memo: product-card, post-card, ideas-card 생성

--- app/common/pages/home-page.tsx
+++ app/common/pages/home-page.tsx
@@ -1,7 +1,8 @@
 import { Link, type MetaFunction } from "react-router";
 import { Button } from "~/common/components";
-import { ProductCard } from "~/common/components/product-card";
-import { ArrowRightIcon } from "lucide-react";
+import { ProductCard } from "~/features/products/components/product-card";
+import { PostCard } from "~/features/community/components/post-card";
+import { IdeasCard } from "~/features/ideas/components/ideas-card";
 
 export const meta : MetaFunction = () => {
     return [
@@ -12,7 +13,37 @@ export const meta : MetaFunction = () => {
 
 export default function HomePage() {
     return (
-      <div className="px-20">
+      <div className="px-20 space-y-40">
+
+          {/* product card */}
+          <div className="grid grid-cols-3 gap-4">
+              <div>
+                  <h2 className="text-5xl font-bold leading-tight tracking-tight">
+                      Today's Products
+                  </h2>
+                  <p className="text-xl font-light text-foreground">
+                      The best products made by our community today.
+                  </p>
+                  <Button variant="link" asChild className="text-lg p-0 text-destructive">
+                      <Link to="/products/leaderboards">
+                          Explore all products &rarr;
+                      </Link>
+                  </Button>
+              </div>
+              {Array.from({ length: 11 }).map((_, index) => (
+                  <ProductCard
+                      key={index}
+                      id={`productId-${index}`}
+                      name="Product Name"
+                      description="Product Description"
+                      commentsCount={12}
+                      viewsCount={12}
+                      votesCount={120}
+                  />
+              ))}
+          </div>
+
+          {/* post card */}
           <div className="grid grid-cols-3 gap-4">
               <div>
                   <h2 className="text-5xl font-bold leading-tight tracking-tight">
@@ -21,24 +52,54 @@ export default function HomePage() {
                   <p className="text-xl font-light text-foreground">
                       The latest discussions from our community.
                   </p>
-                  <Button variant="link" asChild className="text-lg p-0">
+                  <Button variant="link" asChild className="text-lg p-0 text-destructive">
                       <Link to="/community">
-                          Explore All Products
-                          <ArrowRightIcon />
+                          Explore All Products &rarr;
                       </Link>
                   </Button>
               </div>
 
-              {/* product card */}
-              {Array.from({ length: 10 }).map((_, i) => (
-                  <ProductCard
-                      key={i}
-                      id="productId"
-                      name="Product Name"
-                      description="Product Description"
-                      commentsCount={12}
-                      viewsCount={12}
-                      votesCount={120}
+              {Array.from({ length: 11 }).map((_, index) => (
+                  <PostCard
+                      key={index}
+                      id={`postId-${index}`}
+                      title="What is the best producitivity tool?"
+                      author="Nico"
+                      authorAvatarUrl="https://github.com/apple.png"
+                      category="Productivity"
+                      postedAt="12 hours ago"
+                  />
+              ))}
+          </div>
+
+          {/* ideas GPT card */}
+          <div className="grid grid-cols-3 gap-4">
+              <div>
+                  <h2 className="text-5xl font-bold leading-tight tracking-tight">
+                      IdeasGPT
+                  </h2>
+                  <p className="text-xl font-light text-foreground">
+                      Find ideas for your next project.
+                  </p>
+                  <Button variant="link" asChild className="text-lg p-0 text-destructive">
+                      <Link to="/ideas">
+                          Explore All ideas &rarr;
+                      </Link>
+                  </Button>
+              </div>
+
+              {Array.from({ length: 5 }).map((_, index) => (
+                  <IdeasCard
+                      key={index}
+                      id={`ideasId-${index}`}
+                      title="A startup that creates AI-powered generated personal trainer,
+                        delivering customized fitness recommendations and tracking of
+                        progress using a mobile app to track workouts and progress as well
+                        as a website to manage the business."
+                      viewsCount={123}
+                      postedAt="12 hours ago"
+                      likesCount={12}
+                      claimed={index % 2 === 0}
                   />
               ))}
           </div>
---
+++ app/features/community/components/post-card.tsx
@@ -0,0 +1,48 @@
+import { Link } from "react-router";
+import { Card, CardFooter, CardHeader, CardTitle } from "~/common/components/ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "~/common/components/ui/avatar";
+import { Button } from "~/common/components/ui/button";
+
+interface PostCardProps {
+    id: string;
+    title: string;
+    author: string
+    authorAvatarUrl: string;
+    category: string;
+    postedAt: string;
+}
+
+export function PostCard({
+    id,
+    title,
+    author,
+    authorAvatarUrl,
+    category,
+    postedAt,
+}: PostCardProps) {
+    return (
+        <Link to={`/community/${id}`}>
+            <Card className="bg-transparent hover:bg-card/50 transition-colors">
+                <CardHeader className="flex flex-row items-center gap-2">
+                    <Avatar className="size-14">
+                        <AvatarFallback>{author[0]}</AvatarFallback>
+                        {authorAvatarUrl && <AvatarImage src={authorAvatarUrl} />}
+                    </Avatar>
+                    <div className="space-y-2">
+                        <CardTitle>{title}</CardTitle>
+                        <div className="flex flex-row gap-2 leading-tight text-xs text-muted-foreground">
+                            <span>{author}</span>
+                            <span>{category}</span>
+                            <span>{postedAt}</span>
+                        </div>
+                    </div>
+                </CardHeader>
+                <CardFooter className="flex justify-end">
+                    <Button variant="link" asChild className="text-destructive">
+                        Reply &rarr;
+                    </Button>
+                </CardFooter>
+            </Card>
+        </Link>
+    );
+}
