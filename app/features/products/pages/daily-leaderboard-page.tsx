66faa149964d54eb5787575ba572d07680f0acce
cme95 <auddms617@gmail.com>
2025-05-05 12:11:58 +0900

    memo: 파라미터 값 검증(zod) 방법 학습

--- app/features/products/pages/daily-leaderboard-page.tsx
+++ app/features/products/pages/daily-leaderboard-page.tsx
@@ -1,22 +1,69 @@
+import { DateTime } from "luxon";
 import type { Route } from "./+types/leaderboard-page";
+import { data, isRouteErrorResponse } from "react-router";
+import { z } from "zod";
 
-export const meta : Route.MetaFunction = () => {
-    return [
-        { title: "Leaderboards | WeMaKe" },
-        { name: "description", content: "Top products leaderboard" }
-    ];
-}
+const paramsSchema = z.object({
+    year: z.number(),
+    month: z.number(),
+    day: z.number(),
+});
 
-export function loader({ request }: Route.LoaderArgs) {
+export const loader = ({ params }: Route.LoaderArgs) => {
+    const { success, data:parsedData } = paramsSchema.safeParse(params);
+    if (!success) {
+        throw data(
+            {
+                error_code: "Invalid params",
+                message: "Invalid params",
+            },
+            { status: 400 },
+        );
+    }
+
+    const date = DateTime.fromObject(parsedData).setZone("Asia/Seoul");
+
+    if (!date.isValid) {
+        throw new Error("Invalid date");
+        throw data(
+            {
+                error_code: "Invalid date",
+                message: "Invalid date",
+            },
+            { status: 400 },
+        );
+    }
+    const today = DateTime.now().setZone("Asia/Seoul").startOf("day");
+    if (date > today) {
+        throw data(
+            {
+                error_code: "future_date",
+                message: "Future date",
+            },
+            { status: 400 },
+        )
+    }
     return {
-        products: []
-    };
-}
+        date,
+    }
+};
 
-export default function DailyLeaderboardPage({ loaderData }: Route.ComponentProps) {
+export default function DailyLeaderboardPage() {
     return (
-        <div className="space-y-20">
-
+        <div className="container mx-auto px-4 py-8">
+            <h1 className="text-3xl font-bold mb-6">
+                {/*Top Products of {loaderData.month}/{loaderData.day}/{loaderData.year}*/}
+            </h1>
         </div>
     );
+}
+
+export function  ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
+    if (isRouteErrorResponse(error)) {
+        return <div>{error.data.message} / {error.data.error_code}</div>
+    }
+    if (error instanceof Error) {
+        return <div>{error.message}</div>
+    }
+    return <div>Error in leaderboard</div>
 }
