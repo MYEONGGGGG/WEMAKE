0510ff99c5fa470388708ce9c45506be7b7d5cf9
MYEONGGGGG <auddms617@gmail.com>
2025-05-21 13:34:25 +0900

    memo: 7.1 Server Side vs Client Side Auth

--- app/supa-client.ts
+++ app/supa-client.ts
@@ -4,7 +4,6 @@ import type { Database as SupabaseDatabase } from "database.types";
 
 /** Supabase의 타입 안전성과 컬럼 자동 완성 기능을 제공함.
  *  "<Database>" 제네릭은 Supabase 프로젝트의 스키마를 기반으로 타입을 생성해서 적용함.
- *
  *  "database.types" 파일은 Supabase 프로젝트의 모든 테이블/컬럼/타입 정보를 포함한 타입 선언 파일
  * */
 
