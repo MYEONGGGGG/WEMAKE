2d7062a1ce2c7a8681e39caee65ad7bcd9718f7b
MYEONGGGGG <auddms617@gmail.com>
2025-05-21 10:01:19 +0900

    memo: 6.13 Users Posts

--- app/features/users/pages/profile-posts-page.tsx
+++ app/features/users/pages/profile-posts-page.tsx
@@ -1,25 +1,24 @@
+import { getUserPosts } from "../queries";
 import type { Route } from "./+types/profile-posts-page";
 import { PostCard } from "~/features/community/components/post-card";
 
-
-export const meta: Route.MetaFunction = () => {
-    return [
-        { title: "Profile posts page | WeMaKe" }
-    ];
+export const loader = async ({params}: Route.LoaderArgs) => {
+    const posts = await getUserPosts(params.username);
+    return { posts };
 };
 
-export default function profilePostsPage() {
+export default function profilePostsPage({loaderData}: Route.ComponentProps) {
     return(
         <div className="flex flex-col gap-5">
-            {Array.from({length: 5}, (_, index) => (
+            {loaderData.posts.map((post) => (
                 <PostCard
-                    key={`productId-${index}`}
-                    id={`productId-${index}`}
-                    title="What is the best way to learn React?"
-                    author="Steve Jobs"
-                    authorAvatarUrl="https://github.com/apple.png"
-                    category="Programming"
-                    postedAt="8 hours ago"
+                    key={post.post_id}
+                    id={post.post_id}
+                    title={post.title}
+                    author={post.author}
+                    authorAvatarUrl={post.author_avatar}
+                    category={post.topic}
+                    postedAt={post.created_at}
                     expanded
                 />
             ))}
