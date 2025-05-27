ffe9906c0cc2eeac0e9294fd8e9a5b6c475cd738
MYEONGGGGG <auddms617@gmail.com>
2025-05-27 11:11:46 +0900

    memo: 9.5 Notifications Page

--- app/features/users/components/notification-card.tsx
+++ app/features/users/components/notification-card.tsx
@@ -10,24 +10,41 @@ import {
 } from "~/common/components";
 import { EyeIcon } from "lucide-react";
 import { cn } from "~/lib/utils";
+import { Link } from "react-router";
 
 interface NotificationCardProps {
     avatarUrl: string;
     avatarFallback: string;
     userName: string;
-    message: string;
+    type: "follow" | "review" | "reply";
     timestamp: string;
     seen: boolean;
+    productName?: string;
+    payloadId?: number;
+    postTitle?: string;
 }
 
 export function NotificationCard({
     avatarUrl,
     avatarFallback,
     userName,
-    message,
+    type,
     timestamp,
     seen,
+    productName,
+    postTitle,
+    payloadId,
 }: NotificationCardProps) {
+    const getMessage = (type: "follow" | "review" | "reply") => {
+        switch (type) {
+            case "follow":
+                return " followed you.";
+            case "review":
+                return " reviewed your product: ";
+            case "reply":
+                return " replied to your post: ";
+        }
+    };
 
     return (
         <Card className={cn("min-w-[450px]", seen ? "" : "bg-yellow-500/60")}>
@@ -39,7 +56,17 @@ export function NotificationCard({
                 <div>
                     <CardTitle className="text-lg font-bold">
                         <span>{userName}</span>
-                        <span>{message}</span>
+                        <span>{getMessage(type)}</span>
+                        {productName && (
+                            <Button variant={"ghost"} asChild className="text-lg">
+                                <Link to={`/products/${payloadId}`}>{productName}</Link>
+                            </Button>
+                        )}
+                        {postTitle && (
+                            <Button variant={"ghost"} asChild className="text-lg">
+                                <Link to={`/community/${payloadId}`}>{postTitle}</Link>
+                            </Button>
+                        )}
                     </CardTitle>
                     <small className="text-muted-foreground text-sm">
                         {timestamp}
--- app/sql/triggers/notification-triggers.sql
+++ app/sql/triggers/notification-triggers.sql
@@ -1,4 +1,4 @@
-CREATE FUNCTION public.notify_follow()
+CREATE OR REPLACE FUNCTION public.notify_follow()
     RETURNS TRIGGER
     SECURITY DEFINER SET search_path = ''
     LANGUAGE plpgsql
@@ -10,12 +10,13 @@ BEGIN
 END;
 $$;
 
-CREATE TRIGGER notify_follow_trigger
+CREATE OR REPLACE  TRIGGER notify_follow_trigger
     AFTER INSERT ON public.follows
     FOR EACH ROW
 EXECUTE PROCEDURE public.notify_follow();
 
-CREATE FUNCTION public.notify_review()
+
+CREATE OR REPLACE  FUNCTION public.notify_review()
     RETURNS TRIGGER
     SECURITY DEFINER SET search_path = ''
     LANGUAGE plpgsql
@@ -24,18 +25,18 @@ DECLARE
     product_owner uuid;
 BEGIN
     SELECT profile_id INTO product_owner FROM public.products WHERE product_id = NEW.product_id;
-    INSERT INTO public.notifications (type, source_id, target_id)
-    VALUES ('review', NEW.profile_id, product_owner);
+    INSERT INTO public.notifications (type, source_id, target_id. product_id)
+    VALUES ('review', NEW.profile_id, product_owner, NEW.product_id);
     RETURN NEW;
 END;
 $$;
 
-CREATE TRIGGER notify_review_trigger
+CREATE OR REPLACE  TRIGGER notify_review_trigger
     AFTER INSERT ON public.reviews
     FOR EACH ROW
 EXECUTE PROCEDURE public.notify_review();
 
-CREATE FUNCTION public.notify_reply()
+CREATE OR REPLACE  FUNCTION public.notify_reply()
     RETURNS TRIGGER
     SECURITY DEFINER SET search_path = ''
     LANGUAGE plpgsql
@@ -44,13 +45,13 @@ DECLARE
     post_owner uuid;
 BEGIN
     SELECT profile_id INTO post_owner FROM public.posts WHERE post_id = NEW.post_id;
-    INSERT INTO public.notifications (type, source_id, target_id)
-    VALUES ('reply', NEW.profile_id, post_owner);
+    INSERT INTO public.notifications (type, source_id, target_id, post_id)
+    VALUES ('reply', NEW.profile_id, post_owner, NEW.post_id);
     RETURN NEW;
 END;
 $$;
 
-CREATE TRIGGER notify_reply_trigger
+CREATE OR REPLACE  TRIGGER notify_reply_trigger
     AFTER INSERT ON public.post_replies
     FOR EACH ROW
 EXECUTE PROCEDURE public.notify_reply();
