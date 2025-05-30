c77a453c4abd69c6bfd33e80162396e37f6bd741
MYEONGGGGG <auddms617@gmail.com>
2025-05-30 09:41:43 +0900

    memo: 14.2 Vercel Deployment

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "@react-router/dev/config";
+import { vercelPreset } from "@vercel/react-router/vite";
 
 /** 프레임워크 설정 */
 
@@ -6,4 +7,5 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
+  presets: [vercelPreset()],
 } satisfies Config;
